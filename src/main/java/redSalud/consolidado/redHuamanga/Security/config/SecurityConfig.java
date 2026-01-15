69c401e38491a9f6c168863d8017fbc0287053f3
rodmel <rodmel.calderon.27@unsch.edu.pe>
2026-01-15 10:11:44 -0500

    cambio urls coors

--- src/main/java/redSalud/consolidado/redHuamanga/Security/config/SecurityConfig.java
+++ src/main/java/redSalud/consolidado/redHuamanga/Security/config/SecurityConfig.java
@@ -90,8 +90,8 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource configurationSource(){
         CorsConfiguration config = new CorsConfiguration();
-        //config.setAllowedOrigins(List.of("https://taupe-capybara-74e849.netlify.app/"));
-        config.setAllowedOrigins(List.of("http://localhost:4200/",("http://localhost:4000")));
+        config.setAllowedOrigins(List.of("https://sumaqwayra.com/"));
+        //config.setAllowedOrigins(List.of("http://localhost:4200/",("http://localhost:4000")));
         config.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE"));
         config.setAllowedHeaders(List.of("Content-Type", "Authorization"));
         config.setExposedHeaders(Arrays.asList("Content-Type", "Authorization","Another-Header"));
